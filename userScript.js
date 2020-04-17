d78b90d3ad23b413d3af0ea2cd90dae6c26b023d
YoryeNathan <yorye@mindflow-technologies.com>
2020-04-17 13:44:14 +0300

    mip mop

--- userScript.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Awesome Name!
-// @namespace    drunkathon@userscripts.namespace
-// @version      0.1
-// @description  Description
-// @author       Drunkathon
-// @match        TODO
-// @run-at       document-end
-// ==/UserScript==
