0dc7e40e2cf23093b014e86a6fac3e15d34cbff1
YoryeNathan <yorye@mindflow-technologies.com>
2020-04-17 13:26:30 +0300

    init user script

---
+++ userScript.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Awesome Name!
+// @namespace    drunkathon@userscripts.namespace
+// @version      0.1
+// @description  Description
+// @author       Drunkathon
+// @match        TODO
+// @run-at       document-end
+// ==/UserScript==
