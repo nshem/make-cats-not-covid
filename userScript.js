f678c7b530bc3347a45850b27adc3b74b766bb64
YoryeNathan <yorye@mindflow-technologies.com>
2020-04-17 15:01:03 +0300

    correct basic script

--- userScript.js
+++ userScript.js
@@ -1,21 +1,20 @@
 // ==UserScript==
 // @name         make cats not covid
 // @namespace    drunkathon@bloop.namespace
-// @version      0.1
+// @version      0.1.4
 // @description  Does stuff
 // @author       drunkathon@notreal.com
 // @match        https://*/*
 // @run-at       document-end
 // ==/UserScript==
 
-console.log('Hello!')
+console.log('<SCRIPT START>')
 
 const observer = new MutationObserver(mutations => {
-  console.log('Boop')
   for (let m of mutations) {
     const nodes = m.addedNodes || [];
-    for (let n of nodes) {
-    	checkElementsRecursive(n)
+    for (let i = 0; i < nodes.length; i++) {
+    	checkElementsRecursive(nodes[i])
     }
   }
 })
@@ -27,7 +26,9 @@ checkElementsRecursive(document.body)
 function checkElementsRecursive(el) {
   if (!el) return
   checkElementForCorona(el)
-  ;(el.children || []).forEach(checkElementsRecursive)
+  const childs = el.children || []
+  for (let n of childs)
+    checkElementsRecursive(n)
 }
 
 function checkElementForCorona(el) {
